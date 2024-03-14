142f1e127a7988f325dc21d7b005956df62c0050
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2024-03-14 20:39:39 +0000

    feat: Add environment configuration and Google Cloud Storage integration
    - Add `.env.template` with BUCKET_NAME and PROJECT_ID for environment setup.
    - Unignore `Pipfile.lock` in `.gitignore` to ensure consistent dependencies across environments.
    - Update Docker base image to `python:3.12-slim` in `Dockerfile` for the latest security and performance improvements.
    - Introduce `Pipfile` and `requirements.txt` for package management, specifying dependencies and their versions to ensure project consistency.
    - Enhance `src/app.py` to integrate Google Cloud Storage, allowing dynamic storage of company names inputted by users.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,23 @@
+import os
 import streamlit as st
+from google.cloud import storage
 
 def handle_input(company_name):
     # This function will handle the input, you can add your own logic here
     st.write(f'Company Name: {company_name}')
+
+    # Create a storage client    
+    storage_client = storage.Client(project=os.getenv("PROJECT_ID"))
+
+    # The name for the new bucket
+    bucket_name = os.getenv("BUCKET_NAME")  # Get the bucket name from an environment variable
+
+    # Get the bucket
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob("company_name.txt")
+    blob.upload_from_string(company_name)
 
 def main():
     st.title('Company Name Input')
