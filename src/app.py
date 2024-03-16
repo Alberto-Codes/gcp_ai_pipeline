26f96a49ae1c15ff16e4200be3a03c77d928d333
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2024-03-16 12:53:54 -0700

    release/release-0.7-uat
    * refactor: streamline environment variables and enhance Streamlit UI
    
    - Standardize environment variable names for clarity in .env.template.
    - Introduce new variables for separate data stores and AI engines in .env.template.
    - Add Streamlit configuration to improve security and user experience.
    - Update app structure to utilize Streamlit columns, improving layout and interaction.
    - Implement document import functionality for both search-focused and chat-focused data stores.
    - Integrate an AI chat agent in the UI for interactive user engagement.

--- src/app.py
+++ src/app.py
@@ -10,6 +10,9 @@ from PyPDF2 import PdfReader, PdfWriter
 from document_processing.datastore_refresh import import_documents_sample
 from gcp_integration.search_convo import search_sample
 from esg_score_fetch.sasb_fetch import fetch_sasb_pdf_links
+import streamlit.components.v1 as components
+
+
 
 
 def search_pdfs(company_name, api_key, search_engine_id):
@@ -77,84 +80,131 @@ def handle_input(company_name, pdf_urls):
 
 
 def main():
-    if "user_id" not in st.session_state:
-        st.session_state.user_id = str(uuid.uuid4())
 
-    api_key = os.getenv("API_KEY")
-    search_engine_id = os.getenv("SEARCH_ENGINE_ID")
 
-    st.title("Company Name Input")
-    company_name = st.text_input("Enter the company name")
 
-    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
-    pdf_bucket = os.getenv("PDF_BUCKET_NAME")
-    gcs_input_prefix = f"gs://{pdf_bucket}/"
-    pdf_bucket_gcs_uri = f"{gcs_input_prefix}*"
-
-    if st.button("Submit"):
-        pdf_urls = search_pdfs(company_name, api_key, search_engine_id)
-        handle_input(company_name, pdf_urls)
-        sasb_pdf_urls = fetch_sasb_pdf_links(company_name)
-        if sasb_pdf_urls:
-            st.write("Found SASB PDFs:")
-            for url in sasb_pdf_urls:
-                st.write(url)
-        handle_input(company_name, sasb_pdf_urls)
+    col1, col2 = st.columns(2)
+    with col1:
         project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
-        location = "us"  # Values: "global"
+        pdf_bucket = os.getenv("PDF_BUCKET_NAME")
+        gcs_input_prefix = f"gs://{pdf_bucket}/"
+        pdf_bucket_gcs_uri = f"{gcs_input_prefix}*"
+        if "user_id" not in st.session_state:
+            st.session_state.user_id = str(uuid.uuid4())
 
-        # Import the documents into the Datastore
-        st.write("Importing documents into Datastore...")
-        import_documents_sample(
-            project_id=project_id,
-            location=location,
-            data_store_id=os.getenv("DATA_STORE_ID"),
-            gcs_uri=pdf_bucket_gcs_uri,
+        api_key = os.getenv("API_KEY")
+        search_engine_id = os.getenv("SEARCH_ENGINE_ID")
+
+        st.title("Company Name Input")
+        company_name = st.text_input("Enter the company name")
+
+
+
+        if st.button("Submit"):
+            pdf_urls = search_pdfs(company_name, api_key, search_engine_id)
+            handle_input(company_name, pdf_urls)
+            sasb_pdf_urls = fetch_sasb_pdf_links(company_name)
+            if sasb_pdf_urls:
+                st.write("Found SASB PDFs:")
+                for url in sasb_pdf_urls:
+                    st.write(url)
+            handle_input(company_name, sasb_pdf_urls)
+            project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+            location = "us"  # Values: "global"
+
+            # Import the documents into the Datastore
+            st.write("Importing documents into Datastore...")
+            import_documents_sample(
+                project_id=project_id,
+                location=location,
+                data_store_id=os.getenv("SEARCH_PDF_DATA_STORE_ID"),
+                gcs_uri=pdf_bucket_gcs_uri,
+            )
+            import_documents_sample(
+                project_id=project_id,
+                location="global",
+                data_store_id=os.getenv("CHAT_PDF_DATA_STORE_ID"),
+                gcs_uri=pdf_bucket_gcs_uri,
+            )
+        # Get the search query from the user
+        default_search_query = f"What do you know about {company_name} environment sustainability and governance?"
+        search_query = st.text_input(
+            "Please type your search query:", value=default_search_query
         )
-    # Get the search query from the user
-    default_search_query = f"What do you know about {company_name} environment sustainability and governance?"
-    search_query = st.text_input(
-        "Please type your search query:", value=default_search_query
-    )
-    if st.button("Search"):
-        search_response = search_sample(
-            project_id=project_id,
-            location="us",
-            engine_id=os.getenv("AI_ENGINE_ID"),
-            # search_query=f"Please explain ESG or environmental social governance efforts from this company named {company_name}.",
-            search_query=search_query,
+        if st.button("Search"):
+            search_response = search_sample(
+                project_id=project_id,
+                location="us",
+                engine_id=os.getenv("SEARCH_AI_ENGINE_ID"),
+                # search_query=f"Please explain ESG or environmental social governance efforts from this company named {company_name}.",
+                search_query=search_query,
+            )
+
+            # Display the summary text
+            summary_text = search_response.summary.summary_text
+            st.markdown(f"**Summary:** {summary_text}")
+            # Display the total size of the results
+            st.markdown(f"**Total Results:** {search_response.total_size}")
+
+            # Loop through each result
+            for i, result in enumerate(search_response.results, start=1):
+                # Display the result number
+                st.markdown(f"**Result {i}**")
+
+                # Display the document ID
+                st.markdown(f"**Document ID:** {result.id}")
+
+                # Display the document title
+                title = result.document.derived_struct_data.get("title")
+                st.markdown(f"**Title:** {title}")
+
+                # Display the document link
+                link = result.document.derived_struct_data.get("link")
+                st.markdown(f"**Link:** {link}")
+
+                # Display the snippets
+                snippets = result.document.derived_struct_data.get("snippets")
+                for snippet in snippets:
+                    snippet_text = snippet.get("snippet")
+                    st.markdown(f"**Snippet:** {snippet_text}", unsafe_allow_html=True)
+
+        
+    with col2:
+        
+        st.title("Chat with the AI Agent")
+        chat_agent_id = os.getenv('CHAT_AGENT_ID')
+        project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+        components.html(
+                f"""
+                <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
+                <script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+                <df-messenger
+                    project-id="{project_id}"
+                    agent-id="{chat_agent_id}"
+                    language-code="en"
+                    max-query-length="-1">
+                    <df-messenger-chat
+                    chat-title="gcp ai pipeline chat">
+                    </df-messenger-chat>
+                </df-messenger>
+                <style>
+                    df-messenger {{
+                        z-index: 999;
+                        position: fixed;
+                        bottom: 0;
+                        right: 0;
+                        top: 0;
+                        width: 350px;
+                    }}
+                </style>
+                """,
+                height=600
         )
 
-        # Display the summary text
-        summary_text = search_response.summary.summary_text
-        st.markdown(f"**Summary:** {summary_text}")
-        # Display the total size of the results
-        st.markdown(f"**Total Results:** {search_response.total_size}")
+        # More of your Streamlit app code goes here
 
-        # Loop through each result
-        for i, result in enumerate(search_response.results, start=1):
-            # Display the result number
-            st.markdown(f"**Result {i}**")
+        # More of your Streamlit app code goes here
 
-            # Display the document ID
-            st.markdown(f"**Document ID:** {result.id}")
-
-            # Display the document title
-            title = result.document.derived_struct_data.get("title")
-            st.markdown(f"**Title:** {title}")
-
-            # Display the document link
-            link = result.document.derived_struct_data.get("link")
-            st.markdown(f"**Link:** {link}")
-
-            # Display the snippets
-            snippets = result.document.derived_struct_data.get("snippets")
-            for snippet in snippets:
-                snippet_text = snippet.get("snippet")
-                st.markdown(f"**Snippet:** {snippet_text}", unsafe_allow_html=True)
-
-            # Add a separator between results
-            st.markdown("---")
 
 
 if __name__ == "__main__":
